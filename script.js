763092922945afbe8b4ae2ceded488066706d885
Afna Rahim <afna@terrificminds.com>
2022-08-24 10:12:06 +0530

    pagination added

--- script.js
+++ script.js
@@ -223,6 +223,73 @@ function newsfetching(latestNews){
         });
 // js for search bar ends here
 
+// js for pagination
+const element = document.querySelector(".pagination ul");
+
+let totalPages = 864;
+let page = 1;
+
+//calling function with passing parameters and adding inside element which is ul tag
+element.innerHTML = createPagination(totalPages, page);
+function createPagination(totalPages, page){
+  let liTag = '';
+  let active;
+  let beforePage = page -1;
+  let afterPage = page + 1;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+ 
+  if(page > 1){ //show the next button if the page value is greater than 1
+    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"
+    ><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
+  }
+  
+  // how many pages or li show before the current li
+  if (page == totalPages) {
+    beforePage = beforePage - 2;
+  } else if (page == totalPages - 1) {
+    beforePage = beforePage - 1;
+  }
+  // how many pages or li show after the current li
+  if (page == 1) {
+    afterPage = afterPage + 2;
+  } else if (page == 2) {
+    afterPage  = afterPage + 1;
+  }
+
+  for (var plength = beforePage; plength <= afterPage; plength++) {
+    if (plength > totalPages) { //if plength is greater than totalPage length then continue
+      continue;
+    }
+    if (plength == 0) { //if plength is 0 than add +1 in plength value
+      plength = plength + 1;
+    }
+    if(page == plength){ //if page is equal to plength than assign active string in the active variable
+      active = "active";
+    }else{ //else leave empty to the active variable
+      active = "";
+    }
+    liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
+  }
+
+
+
+  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
+    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+  }
+ 
+  pagiing(page);
+  function pagiing(page) {
+let url1="https://newsdata.io/api/1/news?apikey=pub_10316cb54031f0ba55b0468a1337c21dc0bbb&country=au,ca&page="+page+"";
+fetching(url1,newapi); 
+}
+
+  element.innerHTML = liTag; //add li tag inside ul tag
+  return liTag; //reurn the li tag
+}
+
+// js for pagination
+
 
        function openPopup(i) {
       let popup = document.getElementById(`popup${i}`);
