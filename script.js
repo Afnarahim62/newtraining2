df01981f3d8188b889c6985483b8e0cf76d26b9a
Muhammed Rahees <109892155+muhammedrahees360@users.noreply.github.com>
2022-08-25 17:16:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let newapi="pub_10487b1affaf56d8b793d3c9cf14addf974c4";
+let newapi="pub_106034d55252e54cc00f4b6ad3ff0c71c3a41";
 // weather and date js starts here 
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
@@ -37,7 +37,7 @@ function getWeatherData () {
 
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
 
-        console.log(data)
+      
         showWeatherData(data);
         })
 
@@ -76,66 +76,23 @@ function showWeatherData (data){
     weatherForecastEl.innerHTML = otherDayForcast;
 }
 // weather and date js ends here 
+function myFunction1(newapi,a) {
+
+  let name = a.id;
+  
+      let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
+      fetching(url1,newapi); 
+}
 
 
     //  js for navigation bar starts here
-     function myFunction1(newapi) {
-                let name="business";
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
+     function myFunction2(newapi) {
+             
+          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&language=en";
           fetching(url1,newapi); 
 }
 
 
-        
-function myFunction9(newapi) {
-                let name="world";
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
-          fetching(url1,newapi); 
-}
-        
-function myFunction2(newapi) {
-    let name="entertainment";
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
-          fetching(url1,newapi); 
-}
-        
-function myFunction3(newapi) {
-    let name="health";
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
-          fetching(url1,newapi); 
-}
-        
-function myFunction4(newapi) {
-                let name="politics"
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
-          fetching(url1,newapi); 
-}
-        
-function myFunction5(newapi) {
-                let name="science"
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
-          fetching(url1,newapi); 
-}
-        
-function myFunction6(newapi) {
-                let name="sports"
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
-          fetching(url1,newapi); 
-          
-          
-}
-        
-function myFunction7(newapi) {
-                let name="technology"
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
-          fetching(url1,newapi); 
-}
-        
-function myFunction8(newapi) {
-                let name="top"
-          let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&category="+name+"&language=en";
-          fetching(url1,newapi); 
-}
 
 // js for navigation ends here
 // news card fetching starts here
@@ -157,7 +114,7 @@ function myFunction8(newapi) {
         
       let output = "";
       let latestNews = data.results;
-      console.log(latestNews);
+      
       newsfetching(latestNews);
 function newsfetching(latestNews){
       for (var i in latestNews) {
@@ -176,18 +133,19 @@ function newsfetching(latestNews){
              <p>Creator: <i>${(latestNews[i].creator == null) ? ("--------") : (latestNews[i].creator)}</i></p><br>
               <p><b>Description</b>: ${(latestNews[i].description == null) ? ("-------") : (latestNews[i].description)}</p><br>
             
-              <button class="readmore-btn" id=pop-up${i} onclick="openPopup(${i})"  >Read More >></button>
+              
+              <button class="readmore-btn" id=pop-up${i} onclick="openPopup(${i})" style="display:${(latestNews[i].content == null) ? ("none") : ("block")}"><b>Read More</button>
                 </div>
                </div>
           </div>
           <div class="popup" id=popup${i}>
-              <b><h style="font-family: Arial, Helvetica, sans-serif; font-size: 35px;">${latestNews[i].title}</h></b><br><br>
-              <img src="${(latestNews[i].image_url == null) ? ("noimage.jpg") : (latestNews[i].image_url)}" style="width:400px;height:auto;"/> <br><br>
+              <h style="font-family: Arial, Helvetica, sans-serif; font-size: 35px;">${latestNews[i].title}</h><br><br>
+              <img src="${(latestNews[i].image_url == null) ? (" ") : (latestNews[i].image_url)}" style="width:400px;height:auto;"/> <br><br>
                 <p>${(latestNews[i].content == null) ? ("Not Content") : (latestNews[i].content)}</p><br>
-                <button class="close-btn" type="button" style="background-color: #FF0000;
-  padding: 16px 20px;
+                <button class="close-btn" type="button" style="background-color: black;
+                padding: 5px 10px;
   cursor: pointer;
-  border-radius: 10px;" onclick="closePopup(${i})">Close</button>
+  border-radius: 10px;" onclick="closePopup(${i})"><b>Close</b></button>
                 </div>
         `;
       }
@@ -195,7 +153,7 @@ function newsfetching(latestNews){
         $("#newsResults").html(output);
       }
       else{
-        let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>This news isn't available. Sorry about that.<br>Try searching for something else </div>`;
+        let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>Try Something Else</div>`;
          $("#newsResults").html(noNews);
       
       }
@@ -220,12 +178,12 @@ function newsfetching(latestNews){
          
           let url1="https://newsdata.io/api/1/news?apikey="+newapi+"&q="+query+"&language=en";
          
-          console.log(url1);
+          
           if(query !== ""){
             fetching(url1,newapi); 
           }
               else{
-                  let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>Search bar is Empty!!!! Try for something else </div>`;
+                  let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>Please Enter Something</div>`;
                    $("#newsResults").html(noNews);
                 
                 }
@@ -233,80 +191,95 @@ function newsfetching(latestNews){
   
     
         });
-// js for search bar ends here
 
 // js for pagination
 const element = document.querySelector(".pagination ul");
 
-let totalPages = 864;
+// let totalPages = 864;
 let page = 1;
 
 //calling function with passing parameters and adding inside element which is ul tag
-element.innerHTML = createPagination(totalPages, page);
-function createPagination(totalPages, page){
-  let liTag = '';
-  let active;
-  let beforePage = page -1;
-  let afterPage = page + 1;
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
- 
-  if(page > 1){ //show the next button if the page value is greater than 1
-    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"
-    ><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
-  }
-  
-  // how many pages or li show before the current li
-  if (page == totalPages) {
-    beforePage = beforePage - 2;
-  } else if (page == totalPages - 1) {
-    beforePage = beforePage - 1;
-  }
-  // how many pages or li show after the current li
-  if (page == 1) {
-    afterPage = afterPage + 2;
-  } else if (page == 2) {
-    afterPage  = afterPage + 1;
-  }
+function createPagination( totalPages,page){
+let liTag = '';
+let active;
+let beforePage = page -1;
+let afterPage = page + 1;
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
 
-  for (var plength = beforePage; plength <= afterPage; plength++) {
-    if (plength > totalPages) { //if plength is greater than totalPage length then continue
-      continue;
-    }
-    if (plength == 0) { //if plength is 0 than add +1 in plength value
-      plength = plength + 1;
-    }
-    if(page == plength){ //if page is equal to plength than assign active string in the active variable
-      active = "active";
-    }else{ //else leave empty to the active variable
-      active = "";
-    }
-    liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
-  }
+if(page > 1){ //show the next button if the page value is greater than 1
+liTag += `<li class="btn prev" onclick="createPagination(${totalPages}, ${page - 1})"
+><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
+}
 
+// how many pages or li show before the current li
+if (page == totalPages) {
+beforePage = beforePage - 2;
+} else if (page == totalPages - 1) {
+beforePage = beforePage - 1;
+}
+// how many pages or li show after the current li
+if (page == 1) {
+afterPage = afterPage + 2;
+} else if (page == 2) {
+afterPage = afterPage + 1;
+}
 
+for (var plength = beforePage; plength <= afterPage; plength++) {
+if (plength > totalPages) { //if plength is greater than totalPage length then continue
+continue;
+}
+if (plength == 0) { //if plength is 0 than add +1 in plength value
+plength = plength + 1;
+}
+if(page == plength){ //if page is equal to plength than assign active string in the active variable
+active = "active";
+}else{ //else leave empty to the active variable
+active = "";
+}
+liTag += `<li class="numb ${active}" onclick="createPagination(${totalPages}, ${plength})"><span>${plength}</span></li>`;
+}
 
-  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
-  }
- 
-  pagiing(page);
-  function pagiing(page) {
-let url1="https://newsdata.io/api/1/news?apikey=pub_10316cb54031f0ba55b0468a1337c21dc0bbb&country=au,ca&page="+page+"";
+if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
+liTag += `<li class="btn next" onclick="createPagination(${totalPages}, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+}
+
+pagiing(page,newapi);
+function pagiing(page,newapi) {
+let url1="https://newsdata.io/api/1/news?apikey=pub_106034d55252e54cc00f4b6ad3ff0c71c3a41&page="+page+"&language=en";
+
 fetching(url1,newapi); 
 }
+element.innerHTML = liTag; //add li tag inside ul tag
+//reurn the li tag})
 
-  element.innerHTML = liTag; //add li tag inside ul tag
-  return liTag; //reurn the li tag
 }
 
+let totalPages=fetch("https://newsdata.io/api/1/news?apikey=pub_106034d55252e54cc00f4b6ad3ff0c71c3a410&language=en")
+.then(response => response.json())
+.then(data => {
+let totalpage= data.totalResults;
+
+let th=Math.ceil(totalpage/10);
+
+return th;
+
+}
+
+).then(totalPages=>{
+
+// // change
+createPagination( totalPages,page);
+})
+
+// change
 // js for pagination
 
 
        function openPopup(i) {
       let popup = document.getElementById(`popup${i}`);
       popup.classList.add("open-popup");
-      console.log(popup)
+     
 
       
     }
@@ -315,7 +288,7 @@ fetching(url1,newapi);
     function closePopup(i) {
       let popup = document.getElementById(`popup${i}`);
       popup.classList.remove("open-popup");
-      console.log("close pop up")
+     
     }
     
     $(document).mouseup(function(e){
@@ -324,26 +297,18 @@ fetching(url1,newapi);
           $(".popup").removeClass("open-popup"); 
       }
   });
- function wpopup(){
-  console.log("working")
- }
-
-
-
-
 //  weatherpop 
 
 function wpopup() {
   let popup = document.getElementById(`weatherpop`);
   popup.classList.add("openweatherpop");
-  console.log(popup)
 
   
 }
 function closeweatherpopup() {
   let popup = document.getElementById(`weatherpop`);
   popup.classList.remove("openweatherpop");
-  console.log("close pop up")
+  
 }
 $(document).mouseup(function(e){
   var container = $(".weatherpop");
