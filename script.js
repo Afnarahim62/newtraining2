6234dc10cedfc3888742e39d302140df324c85e5
ghanshyam <ghanshyam@terrificminds.com>
2022-08-25 14:56:30 +0530

    search updated

--- script.js
+++ script.js
@@ -152,6 +152,11 @@ function newsfetching(latestNews){
       if(output !==""){
         $("#newsResults").html(output);
       }
+      else{
+        let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>Try Something Else</div>`;
+         $("#newsResults").html(noNews);
+      
+      }
       }
        
        
@@ -178,7 +183,7 @@ function newsfetching(latestNews){
             fetching(url1,newapi); 
           }
               else{
-                  let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>This news isn't available. Sorry about that.<br>Try searching for something else </div>`;
+                  let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>Please Enter Something</div>`;
                    $("#newsResults").html(noNews);
                 
                 }
@@ -186,74 +191,7 @@ function newsfetching(latestNews){
   
     
         });
-// js for search bar ends here
-/*
-// js for pagination
-const element = document.querySelector(".pagination ul");
 
-let totalPages = 864;
-let page = 1;
-
-//calling function with passing parameters and adding inside element which is ul tag
-element.innerHTML = createPagination(totalPages, page);
-function createPagination(totalPages, page){
-  let liTag = '';
-  let active;
-  let beforePage = page -1;
-  let afterPage = page + 1;
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
- 
-  if(page > 1){ //show the next button if the page value is greater than 1
-    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"
-    ><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
-  }
-  
-  // how many pages or li show before the current li
-  if (page == totalPages) {
-    beforePage = beforePage - 2;
-  } else if (page == totalPages - 1) {
-    beforePage = beforePage - 1;
-  }
-  // how many pages or li show after the current li
-  if (page == 1) {
-    afterPage = afterPage + 2;
-  } else if (page == 2) {
-    afterPage  = afterPage + 1;
-  }
-
-  for (var plength = beforePage; plength <= afterPage; plength++) {
-    if (plength > totalPages) { //if plength is greater than totalPage length then continue
-      continue;
-    }
-    if (plength == 0) { //if plength is 0 than add +1 in plength value
-      plength = plength + 1;
-    }
-    if(page == plength){ //if page is equal to plength than assign active string in the active variable
-      active = "active";
-    }else{ //else leave empty to the active variable
-      active = "";
-    }
-    liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
-  }
-
-
-
-  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
-  }
- 
-  pagiing(page);
-  function pagiing(page) {
-let url1="https://newsdata.io/api/1/news?apikey=pub_10487b1affaf56d8b793d3c9cf14addf974c4&country=au,ca&page="+page+"";
-fetching(url1,newapi); 
-}
-
-  element.innerHTML = liTag; //add li tag inside ul tag
-  return liTag; //reurn the li tag
-}
-
-// js for pagination*/
 // js for pagination
 const element = document.querySelector(".pagination ul");
 
